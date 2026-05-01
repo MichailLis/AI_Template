6dd09b3dfc8cda099626b3513a926234d5a6fe6b
MichailLis <morro665065@yandex.ru>
2026-05-01 17:49:16 +0300

    refactor(tests): compact admin tests workspace facade

--- client/src/widgets/admin-tests-workspace/ui/use-admin-tests-workspace.ts
+++ client/src/widgets/admin-tests-workspace/ui/use-admin-tests-workspace.ts
@@ -19,19 +19,9 @@ import { useAdminTestsWorkspaceActions } from './use-admin-tests-workspace-actio
 export type { ListMode } from './use-admin-tests-topics';
 
 export function useAdminTestsWorkspace() {
-  const {
-    activeTopicsQuery,
-    archivedTopicsQuery,
-    topicsQuery,
-    topics,
-    topicsErrorMessage,
-    effectiveSelectedTopicId,
-    navigateToTopic,
-    listMode,
-    setListMode,
-    isSelectedTopicArchived,
-    refetchTopicsOnly,
-  } = useAdminTestsTopics();
+  const topicsState = useAdminTestsTopics();
+  const { activeTopicsQuery, archivedTopicsQuery, effectiveSelectedTopicId, setListMode } =
+    topicsState;
 
   const createTopicMutation = useTestsControllerCreateTopic();
   const createTopicFromAiMutation = useTestsControllerCreateTopicFromAi();
@@ -41,59 +31,31 @@ export function useAdminTestsWorkspace() {
   const reorderQuestionsMutation = useTestsControllerReorderQuestions();
   const publishMutation = useTestsControllerPublishTopic();
 
-  const {
-    detailQuery,
-    detail,
-    draft,
-    detailErrorMessage,
-    draftForm,
-    isDraftDirty,
-    canPublish,
-    draftAutosave,
-    clearDraftEdits,
-    refetchTestsData,
-    updateCurrentDraftEdits,
-    discardCurrentDraftEditsAndResetAutosave,
-  } = useAdminTestsDraft({
+  const mutations = {
+    createTopicMutation,
+    createTopicFromAiMutation,
+    deleteTopicMutation,
+    archiveTopicMutation,
+    restoreTopicMutation,
+    reorderQuestionsMutation,
+    publishMutation,
+  };
+
+  const draftState = useAdminTestsDraft({
     effectiveSelectedTopicId,
     activeTopicsQuery,
     archivedTopicsQuery,
     publishIsPending: publishMutation.isPending,
   });
+  const { discardCurrentDraftEditsAndResetAutosave, isDraftDirty, refetchTestsData } = draftState;
 
   const questionEditor = useQuestionEditor({
     topicId: effectiveSelectedTopicId,
     onDataChanged: refetchTestsData,
   });
 
-  const {
-    newTestTitle,
-    setNewTestTitle,
-    newTestSlug,
-    setNewTestSlug,
-    newTestDescription,
-    setNewTestDescription,
-    testSearch,
-    setTestSearch,
-    isAiGeneratorOpen,
-    setIsAiGeneratorOpen,
-    isPublishConfirmOpen,
-    setIsPublishConfirmOpen,
-    pendingDeleteTopic,
-    setPendingDeleteTopic,
-    pendingTopicSwitchId,
-    setPendingTopicSwitchId,
-    isSwitchConfirmOpen,
-    setIsSwitchConfirmOpen,
-    isNavigationConfirmOpen,
-    setIsNavigationConfirmOpen,
-    pendingNavigationPath,
-    setPendingNavigationPath,
-    pendingArchiveTopic,
-    setPendingArchiveTopic,
-    pendingRestoreTopic,
-    setPendingRestoreTopic,
-  } = useAdminTestsDialogState();
+  const dialogState = useAdminTestsDialogState();
+  const { setIsNavigationConfirmOpen, setPendingNavigationPath } = dialogState;
 
   const handleAttemptNavigation = useCallback(
     (targetPath: string) => {
@@ -149,123 +111,27 @@ export function useAdminTestsWorkspace() {
     ],
   );
 
-  const {
-    handleCreateTest,
-    handleCreateTestFromAi,
-    handleSelectTest,
-    handleConfirmTopicSwitch,
-    handleConfirmDeleteTopic,
-    handleConfirmPublish,
-    handleReorderQuestions,
-    handleConfirmArchiveTopic,
-    handleConfirmRestoreTopic,
-    autosaveHint,
-  } = useAdminTestsWorkspaceActions({
-    newTestTitle,
-    newTestSlug,
-    newTestDescription,
-    setNewTestTitle,
-    setNewTestSlug,
-    setNewTestDescription,
-    setIsAiGeneratorOpen,
-    createTopicMutation,
-    createTopicFromAiMutation,
-    deleteTopicMutation,
-    reorderQuestionsMutation,
-    publishMutation,
-    draftAutosave,
-    refetchTopicsOnly,
-    refetchTestsData,
-    effectiveSelectedTopicId,
-    isDraftDirty,
-    setPendingTopicSwitchId,
-    setIsSwitchConfirmOpen,
-    pendingTopicSwitchId,
-    draft,
-    clearDraftEdits,
-    pendingDeleteTopic,
-    setPendingDeleteTopic,
-    topics,
+  const actions = useAdminTestsWorkspaceActions({
+    ...dialogState,
+    ...mutations,
+    ...draftState,
+    ...topicsState,
     questionEditor,
-    setIsPublishConfirmOpen,
-    detail,
     refetchDetailOnly: () => {
-      void detailQuery.refetch();
+      void draftState.detailQuery.refetch();
     },
-    pendingArchiveTopic,
-    setPendingArchiveTopic,
-    pendingRestoreTopic,
-    setPendingRestoreTopic,
-    archiveTopicMutation,
-    restoreTopicMutation,
-    setListMode,
-    navigateToTopic,
   });
 
   return {
-    topics,
-    topicsQuery,
-    topicsErrorMessage,
-    detail,
-    detailQuery,
-    detailErrorMessage,
-    draftForm,
-    isDraftDirty,
-    canPublish,
-    draftAutosave,
+    ...topicsState,
+    ...draftState,
+    ...dialogState,
+    ...mutations,
     questionEditor,
-    autosaveHint,
-    effectiveSelectedTopicId,
-    newTestTitle,
-    newTestSlug,
-    newTestDescription,
-    testSearch,
-    isAiGeneratorOpen,
-    isPublishConfirmOpen,
-    pendingDeleteTopic,
-    pendingTopicSwitchId,
-    isSwitchConfirmOpen,
-    createTopicMutation,
-    createTopicFromAiMutation,
-    deleteTopicMutation,
-    reorderQuestionsMutation,
-    publishMutation,
-    setTestSearch,
-    setNewTestTitle,
-    setNewTestSlug,
-    setNewTestDescription,
-    setIsAiGeneratorOpen,
-    setPendingDeleteTopic,
-    setPendingTopicSwitchId,
-    setIsSwitchConfirmOpen,
-    setIsPublishConfirmOpen,
-    updateCurrentDraftEdits,
-    discardCurrentDraftEditsAndResetAutosave,
+    ...actions,
     handleAttemptNavigation,
     handleConfirmNavigationLeave,
     handleConfirmNavigationStay,
-    handleCreateTest,
-    handleCreateTestFromAi,
-    handleSelectTest,
-    handleConfirmTopicSwitch,
-    handleConfirmDeleteTopic,
-    handleConfirmPublish,
-    handleReorderQuestions,
-    handleConfirmArchiveTopic,
-    handleConfirmRestoreTopic,
     handleToggleTopicActive,
-    listMode,
-    setListMode,
-    pendingArchiveTopic,
-    pendingRestoreTopic,
-    isSelectedTopicArchived,
-    archiveTopicMutation,
-    restoreTopicMutation,
-    setPendingArchiveTopic,
-    setPendingRestoreTopic,
-    isNavigationConfirmOpen,
-    setIsNavigationConfirmOpen,
-    pendingNavigationPath,
-    setPendingNavigationPath,
   };
 }
